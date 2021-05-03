1cb65c9dc07758750276f226108204013dcfe7f6
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-05-03 21:05:58 +0900

    magic 8 ball 2

--- collatz.py
+++ collatz.py
@@ -1,15 +1,18 @@
 
 
 def collatz(num):
-    terms = []
-    while num != 1:
-        if num % 2 == 0:
-            num = num / 2
-            terms.append(int(num))
-        else:
-            num = 3 * num + 1
-            terms.append(int(num))
-        print(terms)
-    return terms
+    try:
+        terms = []
+        while num != 1:
+            if num % 2 == 0:
+                num = num / 2
+                terms.append(int(num))
+            else:
+                num = 3 * num + 1
+                terms.append(int(num))
+            print(terms)
+        return terms
+    except:
+        print("Input is a non-intenger")
 
-print(collatz(56))
+print(collatz("hello"))
