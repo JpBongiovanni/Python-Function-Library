5db19e57e0540acd5ce07243c4799271b7d3533a
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-05-08 21:32:24 +0900

    import random

--- magic8Ball2.py
+++ magic8Ball2.py
@@ -4,4 +4,8 @@ pets = ['Dog', 'Cat', 'Moose', 'lizard', 'mouse']
 random.shuffle(pets)
 
 print(pets)
-    
+
+answers['It is certain', 'It is decidedly so', 'Yes', 'Reply hazy try again', 'Ask again later', 'Concentrate and ask again', 'My reply is no', 'Outlook not so good', 'Very Doubtful']
+
+random.shuffle(answers)
+
