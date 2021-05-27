565242f35e7b41282cb349aa0dd5e85a0670041c
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-05-27 15:06:10 +0900

    Coin Flip Streak

---
+++ CoinFlipStreaks.py
@@ -0,0 +1,23 @@
+import random
+numberOfStreaks = 0
+H = 0
+T = 0
+
+for experimentNumber in range(10000):
+    if random.randint(0,1) == 0:
+        print('H', end=" ")
+        H = H + 1
+        T = 0
+        if H == 2:
+            numberOfStreaks += 1
+            H = 0 
+    elif random.randint(0,1) == 1:
+        print('T', end=" ")
+        T = T + 1
+        H = 0
+        if T == 2:
+            numberOfStreaks += 1
+
+
+
+print('Chance of streak: %s%%' % (numberOfStreaks / 100))
