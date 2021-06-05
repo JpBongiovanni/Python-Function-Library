24cffaefa9e488e3160ea454cc8d601d933902d6
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-06-05 20:35:41 +0900

    added win conditions

--- ticTacToe.py
+++ ticTacToe.py
@@ -8,8 +8,46 @@ def printBoard(board):
     print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
 def winner():
+    #horizontal Win conditions
     if theBoard['top-L'] == 'X' and theBoard['top-M'] == 'X' and theBoard['top-R'] == 'X':
         print("Player X wins!")
+    if theBoard['mid-L'] == 'X' and theBoard['mid-M'] == 'X' and theBoard['mid-R'] == 'X':
+        print("Player X wins!")
+    if theBoard['low-L'] == 'X' and theBoard['low-M'] == 'X' and theBoard['low-R'] == 'X':
+        print("Player X wins!")
+
+    if theBoard['top-L'] == 'O' and theBoard['top-M'] == 'O' and theBoard['top-R'] == 'O':
+        print("Player O wins!")
+    if theBoard['mid-L'] == 'O' and theBoard['mid-M'] == 'O' and theBoard['mid-R'] == 'O':
+        print("Player O wins!")
+    if theBoard['low-L'] == 'O' and theBoard['low-M'] == 'O' and theBoard['low-R'] == 'O':
+        print("Player O wins!")
+
+    #vertical Win conditions
+    if theBoard['top-L'] == 'X' and theBoard['mid-L'] == 'X' and theBoard['low-L'] == 'X':
+        print("Player X wins!")
+    if theBoard['top-M'] == 'X' and theBoard['mid-M'] == 'X' and theBoard['low-M'] == 'X':
+        print("Player X wins!")
+    if theBoard['top-R'] == 'X' and theBoard['mid-R'] == 'X' and theBoard['low-R'] == 'X':
+        print("Player X wins!")
+
+    if theBoard['top-L'] == 'O' and theBoard['mid-L'] == 'O' and theBoard['low-L'] == 'O':
+        print("Player O wins!")
+    if theBoard['top-M'] == 'O' and theBoard['mid-M'] == 'O' and theBoard['low-M'] == 'O':
+        print("Player O wins!")
+    if theBoard['top-R'] == 'O' and theBoard['mid-R'] == 'O' and theBoard['low-R'] == 'O':
+        print("Player O wins!")
+
+    #diagonal Win conditions
+    if theBoard['top-L'] == 'X' and theBoard['mid-M'] == 'X' and theBoard['low-R'] == 'X':
+        print("Player X wins!")
+    if theBoard['top-R'] == 'X' and theBoard['mid-M'] == 'X' and theBoard['low-L'] == 'X':
+        print("Player X wins!")
+    
+    if theBoard['top-L'] == 'O' and theBoard['mid-M'] == 'O' and theBoard['low-R'] == 'O':
+        print("Player O wins!")
+    if theBoard['top-R'] == 'O' and theBoard['mid-M'] == 'O' and theBoard['low-L'] == 'O':
+        print("Player O wins!")
 
 turn = 'X'
 for i in range(9):
