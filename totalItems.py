2386fca4837302895143ec37e3051b7122aaaa1d
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-06-16 20:27:15 +0900

    fixed total brought

--- totalItems.py
+++ totalItems.py
@@ -1,14 +1,15 @@
 allGuests = {'Jason': {'lasagna': 4, 'bread sticks': 12},
             'Rocco': {'ham sandwiches': 3, 'meatballs': 4},
             'Dyanne': {'dishes': 3, 'cups': 1},
-            'Tim': {'drinks': 4, 'sides': 2}
+            'Tim': {'drinks': 4, 'sides': 2},
+            'Josiah': {'gnocci': 10, 'eggplant': 1}
             }
 
 def totalBrought(guests, item):
     numBrought = 0
     for k, v in guests.items():
         numBrought = numBrought + v.get(item, 0)
-        return numBrought
+    return numBrought
 
 print('Number of things being brought:')
 print(' - Lasagna        ' + str(totalBrought(allGuests, 'lasagna')))  
@@ -18,3 +19,5 @@ print(' - Meatballs      ' + str(totalBrought(allGuests, 'meatballs')))
 print(' - Dishes         ' + str(totalBrought(allGuests, 'dishes'))) 
 print(' - Cups           ' + str(totalBrought(allGuests, 'cups')))
 print(' - drinks         ' + str(totalBrought(allGuests, 'drinks')))
+print(' - Gnocci         ' + str(totalBrought(allGuests, 'gnocci')))
+print(' - Eggplant       ' + str(totalBrought(allGuests, 'eggplant')))
