2fa8d595a38743ee35099fe5950e2241ba282461
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-06-30 20:17:06 -0400

    string manipulation

--- validateInput.py
+++ validateInput.py
@@ -1,6 +1,18 @@
-while True:
-    print('Enter your age:')
-    age = input()
-    if age.isdecimal():
-        break
-    print('Please enter a number for your age.')
+# while True:
+#     print('Enter your age:')
+#     age = input()
+#     if age.isdecimal():
+#         break
+#     print('Please enter a number for your age.')
+
+"""This is a test python program.
+Written by AL Sweig 
+
+This program was designed for p3 not p2.
+"""
+
+def spam():
+    """this is a multiline commment to help explain what a spam() function does."""
+    print('Hello!')
+
+spam()
